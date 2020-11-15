8b0ca9cf0e47c43756e6e5803fd165c6664d1263
Max <gerasimovpost@gmail.com>
2020-11-15 14:33:42 +0400

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import AppRouter from './AppRouter';
 import THEME from './theme';
 import GlobalStyles from './styles/global';
 
 const App = () => {
   return (
     <ThemeProvider theme={THEME}>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-      <Button variant="contained" color="secondary">
-        Secondary
-      </Button>
+      <AppRouter />
     </ThemeProvider>
   );
 };
